01cccf38392accee8bd5200e95fbe78029e7c23e
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-03-08 11:14:19 +0000

    add sign in fuctionality

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
@@ -14,11 +15,31 @@ function SignIn() {
   const navigate = useNavigate();
 
   const onChange = (e) => {
-		setFormData((prevState) => ({
-			...prevState,
-			[e.target.id]: e.target.value,
-		}))
-	};
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate('/');
+      }
+    } catch (error) {
+			console.log(error)
+		}
+  };
 
   return (
     <>
@@ -28,7 +49,7 @@ function SignIn() {
         </header>
 
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type="email"
               className="emailInput"
@@ -66,14 +87,12 @@ function SignIn() {
             </div>
           </form>
 
-					{/* Google OAuth */}
+          {/* Google OAuth */}
 
-					<Link to='/sign-up' className='registerLink'>
-						Sign Up Instead
-					</Link>
-					<div>
-
-					</div>
+          <Link to="/sign-up" className="registerLink">
+            Sign Up Instead
+          </Link>
+          <div></div>
         </main>
       </div>
     </>
