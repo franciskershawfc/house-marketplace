17740b34067de658fc367089a142b6c3b011827d
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-03-09 15:36:21 +0000

    add category pages and fetch data

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -7,6 +7,7 @@ import {
   updateProfile,
 } from 'firebase/auth';
 import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
+import OAuth from '../components/OAuth';
 import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
@@ -113,7 +114,7 @@ function SignUp() {
             </div>
           </form>
 
-          {/* Google OAuth */}
+          <OAuth />
 
           <Link to="/sign-in" className="registerLink">
             Sign In Instead
